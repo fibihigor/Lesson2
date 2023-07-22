75b646857a6b970cb89132f582a586af735dbf4f
igor <fibih.igor94@yandex.ru>
2023-07-22 15:13:40 +0300

    Task №2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 13: Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Пример:
+645 -> 5
+78 -> третьей цифры нет
+326743297 -> 6
+*/
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+
+char[] numberArray = number.ToCharArray();
+if (numberArray.Length >= 3)
+{
+    Console.Write($"{number} -> {numberArray[2]}");
+}
+else
+{
+    Console.Write($"{number} -> третьей цифры нет");
+}
